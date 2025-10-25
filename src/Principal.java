3f6b8d4c7b8bf34f3dcd412dacde232ffd4def7b
fcampo92-dev <facundo.damian.campo@gmail.com>
2025-10-24 23:15:49 -0300

    Se cumplio con lo minimo pedido en el CRUD se refinara el funcionamiento de los metodos y las validacion de los datos

--- src/Principal.java
+++ src/Principal.java
@@ -20,7 +20,8 @@ public class Principal {
       System.out.println("2) Listar Cursos");
       System.out.println("3) Busca Curso por ID");
       System.out.println("4) Buscar Curso por Título");
-      System.out.println("5) Eliminar un Curso");
+      System.out.println("5) Modificar Titulo de Curso");
+      System.out.println("6) Eliminar un Curso");
       System.out.println("0) Salir");
       System.out.print("Elige una opción: ");
 
@@ -29,8 +30,10 @@ public class Principal {
       switch (opcion) {
         case "1" -> agregarCurso(cursosDB); //crear();
         case "2" -> listarCursos(cursosDB); // listar();
-        case "3" -> System.out.println("Buscar"); //buscar();
-        case "4" -> System.out.println("Eliminar"); //eliminar();
+        case "3" -> buscarCursosPorID(cursosDB); //buscarID();
+        case "4" -> buscarCursosPorTitulo(cursosDB); //buscarTitulo();
+        case "5" -> modificarCurso(cursosDB); //eliminar();
+        case "6" -> borrarCurso(cursosDB); //eliminar();
         case "0" -> {
           System.out.println("Hasta luego!");
           return;
@@ -51,7 +54,6 @@ public class Principal {
     }
     // TODO: limpiar la pantalla de la consola
   }
-
 
   public static void agregarCurso(ArrayList<Curso> cursosDB){
     System.out.println("=================================================================================================================");
@@ -88,14 +90,117 @@ public class Principal {
           "ID", "TITULO", "NIVEL", "IDIOMA", "DURACION", "PRECIO");
     }
 
-    for(Curso cursos : cursosDB){
-      System.out.printf("| %-3s | %-35s | %-15s | %-15s | %-15s | %-10s |%n",
-          cursos.obtenerId(), cursos.obtenerTitulo(), cursos.obtenerNivel(), cursos.obtenerIdioma(), cursos.obtenerDuracionHoras()+"Hs", "$"+cursos.obtenerPrecio());
+    if(cursosDB != null && !cursosDB.isEmpty()) {
+      for (Curso cursos : cursosDB) {
+        System.out.printf("| %-3s | %-35s | %-15s | %-15s | %-15s | %-10s |%n",
+            cursos.obtenerId(), cursos.obtenerTitulo(), cursos.obtenerNivel(),
+            cursos.obtenerIdioma(), cursos.obtenerDuracionHoras() + "Hs",
+            "$" + cursos.obtenerPrecio());
+      }
+      pausa();
     }
-    pausa();
   }
 
+  public static void buscarCursosPorTitulo (ArrayList<Curso> cursosDB){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese el título de un Curso: ");
+    String busqueda = scanner.nextLine();
+    ArrayList<Curso> cursosEncontrados = new ArrayList<>();
 
+    for (Curso curso : cursosDB){
+      if (estaIncluido(curso.obtenerTitulo(), busqueda)) {
+        cursosEncontrados.add(curso);
+      }
+    }
+
+    listarCursos(cursosEncontrados);
+  }
+
+  public static boolean estaIncluido(String tituloCompleto, String nombreParcial) {
+    String tituloCompletoFormateado = formatoBusqueda(tituloCompleto);
+
+    return tituloCompletoFormateado.contains(formatoBusqueda(nombreParcial));
+  }
+
+  public static String formatoBusqueda(String texto) {
+    return texto.trim().toLowerCase();
+  }
+
+  public static void buscarCursosPorID (ArrayList<Curso> cursosDB){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese el ID de un Curso: ");
+    int busqueda = scanner.nextInt();
+    ArrayList<Curso> cursosEncontrados = new ArrayList<>();
+
+    // TODO: Modificar el imprimir para un solo producto, no es necesario recorrer todo si busco por ID
+    for (Curso curso : cursosDB){
+      if (curso.obtenerId() == busqueda) {
+        cursosEncontrados.add(curso);
+      }
+    }
+
+    listarCursos(cursosEncontrados);
+
+  }
+
+  public static void modificarCurso(ArrayList<Curso> cursosDB) {
+    Curso cursoAModificar = obtenerCurso(cursosDB);
+    // TODO: validar que encontramos el producto
+    if (cursoAModificar == null) {
+      System.out.println("No se puede editar el producto.");
+      pausa();
+      return; // cuando hacemos el return en una funcion void, estamos cortando la ejecucion de la funcion
+    }
+
+    String tituloOriginal = cursoAModificar.obtenerTitulo();
+    System.out.print("Curso a editar el título: ");
+    System.out.println("[ " + tituloOriginal + " ]");
+    // TODO: validar que el usuario quiere editar el producto que se encontro
+    System.out.print("Ingrese el nuevo nombre: ");
+    Scanner sc = new Scanner(System.in);
+    String nuevoTitulo = sc.nextLine();
+
+    cursoAModificar.establecerTitulo(nuevoTitulo);
+
+    System.out.printf("El título del Curso cambio de '%s' a '%s'\n", tituloOriginal, nuevoTitulo);
+  }
+
+  public static Curso obtenerCurso (ArrayList<Curso> cursosDB){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese el ID de un Curso: ");
+    int busqueda = scanner.nextInt();
+    ArrayList<Curso> cursosEncontrados = new ArrayList<>();
+
+    // TODO: Modificar el imprimir para un solo producto, no es necesario recorrer todo si busco por ID
+    for (Curso curso : cursosDB){
+      if (curso.obtenerId() == busqueda) {
+        cursosEncontrados.add(curso);
+      }
+      return curso;
+    }
+
+
+
+    return null;
+  }
+
+  public static void borrarCurso(ArrayList<Curso> cursosDB) {
+    Curso cursoABorrar = obtenerCurso(cursosDB);
+    // TODO: validar que encontramos el producto
+    if (cursoABorrar == null) {
+      System.out.println("No pudimos borrar el producto");
+      pausa();
+      return; //
+    }
+    String nombreOriginal = cursoABorrar.obtenerTitulo();
+    System.out.println("Curso a borrar:");
+    System.out.println(nombreOriginal);
+    // TODO: validar que el usuario quiere borrar el producto que se encontro
+
+    cursosDB.remove(cursoABorrar);
+    System.out.println("Borrado exitosamente!");
+    pausa();
+  }
 
   public static ArrayList<Curso> obtenerCursos(){
     ArrayList <Curso> cursos = new ArrayList<>();
